11df2ac4654bb7e2ce0254e34a4dfb10d383e9df
bittu <bittu@codewhithbittu.com>
2026-01-21 19:58:42 +0530

    Initial commit

---
+++ project/fast.js
@@ -0,0 +1,8 @@
+var typed = new typed(".text",{
+    Strings:["Frontend Developer" , "YouTuber" , "Wed Developre"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
