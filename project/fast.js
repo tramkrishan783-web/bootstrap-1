4c0cd3abb2c379f1b79380b9711ba12e26da116e
tramkrishan783-web <tramkrishan783@gmail.com>
2026-01-22 18:12:02 +0530

    Developr-Designer

--- project/fast.js
+++
@@ -1,8 +0,0 @@
-var typed = new typed(".text",{
-    Strings:["Frontend Developer" , "YouTuber" , "Wed Developre"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-
-});
